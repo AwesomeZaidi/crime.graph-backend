18f8c557f498af3e190286221872702e8abc5fd2
Asim Zaidi <asim_zaidi@apple.com>
2023-04-24 15:44:52 -0500

    dumpy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-    apps: [
-      {
-        name: 'CrimeGraph',
-        script: 'fetch_arrests.js',
-        cron_restart: '0 0 * * *', // Run the script at midnight every day
-        autorestart: false,
-      },
-    ],
-  };
-  
+  apps: [
+    {
+      name: 'CrimeGraph',
+      script: 'fetch_arrests.js',
+      autorestart: false,
+    },
+  ],
+};
